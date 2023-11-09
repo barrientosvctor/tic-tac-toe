015464ae52f477d140954cabe4671579e0c00c60
barrientosvctor <vibarrientosp@gmail.com>
2023-11-08 22:23:07 -0500

    feat: print_table function

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-    puts("Tic tac toe. WIP.");
+void print_table(int (*table)[3][3]);
 
+int main(void) {
+    int game_table[3][3] = {
+        { 0, 0, 0 },
+        { 0, 0, 0 },
+        { 0, 0, 0 }
+    };
+
+    print_table(&game_table);
     return 0;
+}
+
+void print_table(int (*table)[3][3]) {
+ printf(" %d | %d | %d\n", (*table)[0][0], (*table)[0][1], (*table)[0][2]);
+ puts("-----------");
+ printf(" %d | %d | %d\n", (*table)[1][0], (*table)[1][1], (*table)[1][2]);
+ puts("-----------");
+ printf(" %d | %d | %d\n", (*table)[2][0], (*table)[2][1], (*table)[2][2]);
 }
