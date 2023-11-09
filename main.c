83883f9cb1e64368e8ba5bffde7af1845597a7b2
barrientosvctor <vibarrientosp@gmail.com>
2023-11-08 23:13:05 -0500

    style: code spaces

--- main.c
+++ main.c
@@ -14,9 +14,9 @@ int main(void) {
 }
 
 void print_table(int (*table)[3][3]) {
- printf(" %d | %d | %d\n", (*table)[0][0], (*table)[0][1], (*table)[0][2]);
- puts("-----------");
- printf(" %d | %d | %d\n", (*table)[1][0], (*table)[1][1], (*table)[1][2]);
- puts("-----------");
- printf(" %d | %d | %d\n", (*table)[2][0], (*table)[2][1], (*table)[2][2]);
+    printf(" %d | %d | %d\n", (*table)[0][0], (*table)[0][1], (*table)[0][2]);
+    puts("-----------");
+    printf(" %d | %d | %d\n", (*table)[1][0], (*table)[1][1], (*table)[1][2]);
+    puts("-----------");
+    printf(" %d | %d | %d\n", (*table)[2][0], (*table)[2][1], (*table)[2][2]);
 }
