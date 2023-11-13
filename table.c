ccc1151f0ba87f1ced1a2b4630f29d39c40c863c
barrientosvctor <vibarrientosp@gmail.com>
2023-11-13 16:56:39 -0500

    feat(header): functions to table.h

--- table.c
+++ table.c
@@ -1,10 +1,23 @@
 #include "table.h"
 #include <stdio.h>
+#include <stdbool.h>
 
+// TODO: put x-y numbers in each box
+//TODO: check if a box has been filled before
+// TODO: check when entire table is filled
 void print_table(int (*table)[3][3]) {
+    printf("\n");
     printf(" %d | %d | %d\n", (*table)[0][0], (*table)[0][1], (*table)[0][2]);
     puts("-----------");
     printf(" %d | %d | %d\n", (*table)[1][0], (*table)[1][1], (*table)[1][2]);
     puts("-----------");
     printf(" %d | %d | %d\n", (*table)[2][0], (*table)[2][1], (*table)[2][2]);
+    printf("\n");
+}
+
+void check_winner(Player *player1, Player *player2, int (*table)[3][3]) {
+    if (is_winner(player1, table))
+        player1->is_winner = true;
+    else if (is_winner(player2, table))
+        player2->is_winner = true;
 }
--- table.h
+++ table.h
@@ -1,3 +1,6 @@
 #pragma once
 
+#include "player.h"
+
 void print_table(int (*table)[3][3]);
+void check_winner(Player *, Player *, int (*)[3][3]);
