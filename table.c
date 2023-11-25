26022660d6b6422c03e68479833a98767bf9fc82
barrientosvctor <vibarrientosp@gmail.com>
2023-11-25 12:12:31 -0500

    feat(table): is_filled function

--- table.c
+++ table.c
@@ -1,23 +1,34 @@
 #include "table.h"
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 // TODO: put x-y numbers in each box
-//TODO: check if a box has been filled before
-// TODO: check when entire table is filled
+// TODO: check if a box has been filled before
+// TODO: function to format the player's plays
 void print_table(int (*table)[3][3]) {
-    printf("\n");
-    printf(" %d | %d | %d\n", (*table)[0][0], (*table)[0][1], (*table)[0][2]);
-    puts("-----------");
-    printf(" %d | %d | %d\n", (*table)[1][0], (*table)[1][1], (*table)[1][2]);
-    puts("-----------");
-    printf(" %d | %d | %d\n", (*table)[2][0], (*table)[2][1], (*table)[2][2]);
-    printf("\n");
+  printf("\n");
+  printf(" %d | %d | %d\n", (*table)[0][0], (*table)[0][1], (*table)[0][2]);
+  puts("-----------");
+  printf(" %d | %d | %d\n", (*table)[1][0], (*table)[1][1], (*table)[1][2]);
+  puts("-----------");
+  printf(" %d | %d | %d\n", (*table)[2][0], (*table)[2][1], (*table)[2][2]);
+  printf("\n");
 }
 
 void check_winner(Player *player1, Player *player2, int (*table)[3][3]) {
-    if (is_winner(player1, table))
-        player1->is_winner = true;
-    else if (is_winner(player2, table))
-        player2->is_winner = true;
+  if (is_winner(player1, table))
+    player1->is_winner = true;
+  else if (is_winner(player2, table))
+    player2->is_winner = true;
+}
+
+bool is_filled(int (*table)[3][3], int player1_turn_id, int player2_turn_id) {
+  for (int vertical = 0; vertical < 3; vertical++) {
+    for (int horizontal = 0; horizontal < 3; horizontal++) {
+      if ((*table)[vertical][horizontal] == 0)
+        return false;
+    }
+  }
+
+  return true;
 }
--- table.h
+++ table.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "player.h"
+#include <stdbool.h>
 
-void print_table(int (*table)[3][3]);
+void print_table(int (*)[3][3]);
 void check_winner(Player *, Player *, int (*)[3][3]);
+bool is_filled(int (*)[3][3], int, int);
