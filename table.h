4eae5d4585b85a44a5911e62bf481c9b012f0141
barrientosvctor <vibarrientosp@gmail.com>
2023-11-27 23:31:28 -0500

    fix(types): format_box function's return data type

--- table.h
+++ table.h
@@ -10,5 +10,5 @@ void print_table(int (*)[3][3]);
 void check_winner(Player *, Player *, int (*)[3][3]);
 bool is_filled(int (*)[3][3]);
 bool is_box_filled(int (*)[3][3], int, int);
-char format_box(int);
+const char format_box(int);
 bool has_reached_table_size(const int, const int, int, int);
