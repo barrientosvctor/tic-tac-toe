950e6ed8ea0fbd3976719b8f9f2bc637edfd55fd
Victor <vibarrientosp@gmail.com>
2026-02-15 11:57:21 -0500

    Project restructure

---
+++ include/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <raylib.h>
+
+#define RED_LOW_OPACITY (Color){0xFF, 0x0, 0x0, 0xAA}
+
+#endif // !UTILS_H
--- utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <raylib.h>
-
-#define RED_LOW_OPACITY (Color){0xFF, 0x0, 0x0, 0x0A}
-
-#endif
